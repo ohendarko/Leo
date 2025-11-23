5ef08dc10ea37491d0cac5d7f291f27cf348e501
ohendarko <ohendarko@gmail.com>
2025-11-22 23:00:11 -0500

    fixed metadata with proper draft

--- leoai/app/layout.tsx
+++ leoai/app/layout.tsx
@@ -29,10 +29,53 @@ const cinzel = Cinzel({
 })
 
 export const metadata: Metadata = {
-  title: "Leo",
-  description: "A rigorous digital health startup analyst",
+  title: "Leo AI | Digital Health Startup Analyst",
+  description:
+    "The Leo HealthTech Analyst is an AI-powered strategic auditing tool designed to drastically reduce the high failure rate of digital health startups, which often fail due to flawed strategy and implementation rather than poor technology.",
+  keywords: [
+    "Kwadwo Ohene Darko",
+    "",
+    "Digital Health Startup",
+    "AI",
+    "AI Analyst",
+    "Health Informatics",
+    "Full Stack Developer",
+    "Front-end Developer",
+    "Web Developer",
+    "Next.js Developer",
+    "React Developer",
+  ],
+  authors: [{ name: "Kwadwo Ohene Darko" }],
+  openGraph: {
+    title: "Leo AI | Digital Health Startup Analyst",
+    description:
+      "The Leo HealthTech Analyst is an AI-powered strategic auditing tool designed to drastically reduce the high failure rate of digital health startups.",
+    url: "https://leo-phi-olive.vercel.app/",
+    siteName: "Leo AI",
+    images: [
+      {
+        url: "https://leo-phi-olive.vercel.app/leologo.png", // create one with your name/photo
+        width: 600,
+        height: 600,
+        alt: "Kwadwo Ohene Darko Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Leo AI | Digital Health Startup Analyst",
+    description:
+      "The Leo HealthTech Analyst is an AI-powered strategic auditing tool designed to drastically reduce the high failure rate of digital health startups.",
+    creator: "@ohene_kwadwo", // optional
+    images: ["https://leo-phi-olive.vercel.app/leologo.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  metadataBase: new URL("https://leo-phi-olive.vercel.app"),
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
